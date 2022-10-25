3a03b3f8585e4269ba0e55581ac2d00e0e899972
llp <252539670@qq.com>
2022-10-25 11:35:55 +0800

    11

--- llp-mybatisplus/src/main/java/com/llp/mybatisplus/mapper/CarMapper.java
+++
@@ -1,7 +0,0 @@
-package com.llp.mybatisplus.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.llp.mybatisplus.entity.Car;
-
-public interface CarMapper extends BaseMapper<Car> {
-}
