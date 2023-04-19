52379bb3555bca44461f2fcf6008409ed2808354
llp <252539670@qq.com>
2023-04-19 10:12:54 +0800

    SpringBoot动态定时任务

--- llp-schedule/src/main/java/com/llp/schedule/controller/TestController.java
+++ llp-schedule/src/main/java/com/llp/schedule/controller/TestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final ScheduleTask scheduleTask;
+
     private final IntervalScheduleTask intervalScheduleTask;
 
     @GetMapping("/updateCron")
